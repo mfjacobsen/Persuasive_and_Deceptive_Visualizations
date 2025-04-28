1ff1cf9802848fc831fe72133f49363b4160bb0d
mfjacobsen <mfjacobsen12@gmail.com>
2025-04-28 11:30:12 -0700

    Initial Commit

---
+++ global.js
@@ -0,0 +1,20 @@
+function loadPlot(divId, filePath) {
+    fetch(filePath)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to load ${filePath}: ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(fig => {
+            Plotly.newPlot(divId, fig.data, fig.layout);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadPlot('plot1', 'figures/against_nypd_misconduct.json');
+    loadPlot('plot2', 'figures/for_nypd_misconduct.json');
+});
