b25e5a1ae2b3da4aabb4cde79f1870a410b67040
mfjacobsen <mfjacobsen12@gmail.com>
2025-04-28 12:57:04 -0700

    Layout Updates

--- global.js
+++ global.js
@@ -7,6 +7,10 @@ function loadPlot(divId, filePath) {
             return response.json();
         })
         .then(fig => {
+            if (fig.layout) {
+                delete fig.layout.width;
+                delete fig.layout.height;
+            }
             Plotly.newPlot(divId, fig.data, fig.layout);
         })
         .catch(error => {
